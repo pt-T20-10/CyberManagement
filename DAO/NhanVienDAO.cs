975dd47696b2ba093da807285b8c6b4b58d9aca1
nhnghia214 <nhoangnghia2104@gmail.com>
2025-02-14 07:39:22 +0700

    dai lam

--- DAO/NhanVienDAO.cs
+++ DAO/NhanVienDAO.cs
@@ -30,7 +30,7 @@ namespace CyberManagementProject.DAO
         {
             List<NhanVienDTO> nhanvienList = new List<NhanVienDTO>();
 
-            DataTable data = DataProvider.Instance.ExcuteQuery(@" EXEC USP_DanhSachNha  nVien ");
+            DataTable data = DataProvider.Instance.ExcuteQuery(@" EXEC USP_DanhSachNhanVien ");
 
             foreach (DataRow item in data.Rows)
             {
